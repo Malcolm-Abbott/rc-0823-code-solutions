60e45af70cb16f894eacc0a7f01fda4a8862c3e2
Malcolm Abbott <mjacoding@gmail.com>
2023-08-29 18:55:59 +0000

    Completed javascript-data-model exercise Malcolm Abbott

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,53 @@
+const orderHistory = [
+  {
+    title: 'JavaScript for impatient programmers',
+    price: 31.55,
+    orderPlaced: 'August 4, 2020',
+    total: 34.0,
+    shipTo: 'JS Masher',
+    orderNumber: '114 - 3941689 - 8772232',
+    orderDelivered: 'August 8, 2020',
+  },
+  {
+    title: 'The Timeless Way of Building',
+    price: 41.33,
+    orderPlaced: 'July 19, 2020',
+    total: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: '113 - 9984268 - 1280257',
+    orderDelivered: 'July 20, 2020',
+  },
+  {
+    title:
+      'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    price: 15.98,
+    orderPlaced: 'July 4, 2020',
+    total: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '114 - 2875557 - 9059409',
+    orderDelivered: 'July 7, 2020',
+  },
+  [
+    {
+      title:
+        'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+      price: 95.95,
+      orderPlaced: 'July 3, 2020',
+      total: 138.93,
+      shipTo: 'JS Masher',
+      orderNumber: '113 - 2883177 - 2648248',
+      orderDelivered: 'July 5, 2020',
+    },
+    {
+      title: 'The Art of Sql',
+      price: 33.99,
+      orderPlaced: 'July 3, 2020',
+      total: 139.93,
+      shipTo: 'JS Masher',
+      orderNumber: '113 - 2883177 - 2648248',
+      orderDelivered: 'July 5, 2020',
+    },
+  ],
+];
+
+console.log('orderHistory:', orderHistory);
