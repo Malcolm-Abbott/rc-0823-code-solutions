06331257590fb662fd83a89563af1a39f1d9a0bd
Malcolm Abbott <mjacoding@gmail.com>
2023-08-31 21:40:51 +0000

    Completed string-manipulation-1 exercise Malcolm Abbott

---
+++ string-manipulation-1/src/get-first-char.js
@@ -0,0 +1,13 @@
+/* exported getFirstChar */
+
+/*
+PSEUDOCODE:
+
+Define a function named getFirstChar that takes one parameter string.
+Use a return statement to return the value of the string character at index 0.
+
+*/
+
+function getFirstChar(string) {
+  return string[0];
+}
---
+++ string-manipulation-1/src/get-last-char.js
@@ -0,0 +1,10 @@
+/* exported getLastChar */
+
+/*
+Define a function named getLastChar that takes one parameter a string.
+access and return the last index element of the string
+
+*/
+function getLastChar(string) {
+  return string.at(-1);
+}
